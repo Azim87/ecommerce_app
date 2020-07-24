5db19b9f113503756310e9b73fd20421136160fd
unknown <you@example.com>
2020-07-24 18:53:29 +0600

    design of welcome and login page

---
+++ app/src/main/java/kg/azimus/ecommerce/LoginActivity.kt
@@ -0,0 +1,22 @@
+package kg.azimus.ecommerce
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, LoginActivity::class.java))
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/kg/azimus/ecommerce/MainActivity.kt
+++ app/src/main/java/kg/azimus/ecommerce/MainActivity.kt
@@ -1,11 +1,19 @@
 package kg.azimus.ecommerce
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        welcome_btn.setOnClickListener {
+            LoginActivity.start(this)
+        }
     }
 }
